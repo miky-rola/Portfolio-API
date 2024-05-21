95888fbc60a57ced0909f9b1138a0afb01dbe659
miky-rola <mikyrola8@gmail.com>
2024-05-21 02:31:14 +0000

    Add comments and docstrings to views.py

--- myportfolio/portfolio/views.py
+++ myportfolio/portfolio/views.py
@@ -5,16 +5,19 @@ from .models import Project
 from .serializers import ProjectSerializer
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
-
 @api_view(["GET", "POST"])
 def project_list(request):
+    """
+    List all projects or create a new project.
+    """
     if request.method == "GET":
+        # Retrieve all projects
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
     
     elif request.method == "POST":
+        # Create a new project
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -23,13 +26,19 @@ def project_list(request):
 
 @api_view(["GET", "PUT", "DELETE"])
 def project_detail(request, pk):
+    """
+    Retrieve, update or delete a project instance.
+    """
+    # Get the project instance or return 404 if not found
     project = get_object_or_404(Project, pk=pk)
 
     if request.method == "GET":
+        # Retrieve a specific project
         serializer = ProjectSerializer(project)
         return Response(serializer.data)
 
     elif request.method == "PUT":
+        # Update a specific project
         serializer = ProjectSerializer(project, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -37,5 +46,6 @@ def project_detail(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
+        # Delete a specific project
         project.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
